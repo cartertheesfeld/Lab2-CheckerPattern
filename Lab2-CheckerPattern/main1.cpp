3b866a0fa4a73def549edc71cfdb867f89c91d77
cartertheesfeld <cartertheesfeld@hotmail.com>
2022-09-01 10:13:11 -0500

    Add project files.

---
+++ Lab2-CheckerPattern/main1.cpp
@@ -0,0 +1,30 @@
+/*
+	Carter Theesfeld
+	C++ Fall 2022
+	Due: September 14, 2022
+	Lab Name: Lab 2 Exploring Output 
+	Description: This lab is designed to get you familiar output to the command prompt 
+	using C++. 
+	*/
+
+
+#include <iostream>
+#include <string>
+int main()
+{
+	const std::string space = " * * * * ";
+	const std::string star = "* * * * ";
+
+	std::cout << space;
+	std::cout << std::endl << star;
+	std::cout << std::endl << space;
+	std::cout << std::endl << star;
+	std::cout << std::endl << space;
+	std::cout << std::endl << star;
+	std::cout << std::endl << space;
+	std::cout << std::endl << star;
+
+
+
+
+}
